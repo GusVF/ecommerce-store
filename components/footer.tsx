da5b5322e324f4c949d7f20860b9754fe71f3ce4
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-12-01 11:24:10 -0300

    working on navbar.

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-sm text-black">
+          &copy; Tecido em Arte, inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+};
+
+export default Footer;
