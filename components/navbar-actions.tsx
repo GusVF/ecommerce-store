1a93c51ad7d479288f8161b41442335f61d1435a
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-12-12 15:58:20 -0300

    Summary, cartItem, and button disabled added

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -5,6 +5,7 @@ import { ShoppingBag } from 'lucide-react';
 import Button from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import useCart from "@/hooks/use-cart";
+import { useRouter } from "next/navigation";
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -13,13 +14,14 @@ const NavbarActions = () => {
     setIsMounted(true);
   }, []);
 
+  const router = useRouter();
   const cart = useCart();
 
   if (!isMounted) return null;
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag 
           size={20}
           color="white"
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -9,6 +9,7 @@ import { Product } from "@/types";
 import IconButton from "./icon-button";
 import Currency from "./currency";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
   data: Product
@@ -18,6 +19,7 @@ const ProductCard: React.FC<ProductCard> = ({
   data
 }) => {
 
+  const cart = useCart();
   const previewModal = usePreviewModal();
   const router = useRouter();
 
@@ -29,6 +31,12 @@ const ProductCard: React.FC<ProductCard> = ({
     event.stopPropagation();
 
     previewModal.onOpen(data)
+  };
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
   };
 
   return (
@@ -46,10 +54,10 @@ const ProductCard: React.FC<ProductCard> = ({
             <IconButton 
               onClick={onPreview}
               icon={<Expand  size={20} className="text-gray-600"/>}
-              className="I HAVE TO PUT SOMETHING HERE?"
+              className=""
             />
             <IconButton 
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600"/>}
               className=""
             />
