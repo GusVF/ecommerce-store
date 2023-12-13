fcad5c5f6c9f17247abe43d342b95a27407b54b5
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-12-13 11:38:34 -0300

    fixing Modal button

--- components/info.tsx
+++ components/info.tsx
@@ -6,6 +6,7 @@ import { ShoppingCart } from "lucide-react";
 import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
@@ -14,6 +15,11 @@ interface InfoProps {
 const Info: React.FC<InfoProps> = ({
   data
 }) => {
+  const cart = useCart();
+  const onAddToCart = () => {
+    cart.addItem(data)
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">
@@ -40,7 +46,7 @@ const Info: React.FC<InfoProps> = ({
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add to cart
           <ShoppingCart />
         </Button>
